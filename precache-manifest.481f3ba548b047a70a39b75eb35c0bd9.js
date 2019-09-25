70244ffe26e4faeea382d222d71863ed778dfcfe
nco71 <chauxcedric@gmail.com>
2019-09-25 03:39:45 +0000

    Updates

---
+++ precache-manifest.481f3ba548b047a70a39b75eb35c0bd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe3e0e5e77529f7eef65e4524882c1f",
+    "url": "/react-udemy/index.html"
+  },
+  {
+    "revision": "65a7704afd6c1827d693",
+    "url": "/react-udemy/static/css/main.aef39485.chunk.css"
+  },
+  {
+    "revision": "3d4013e813798f1c845c",
+    "url": "/react-udemy/static/js/2.54153103.chunk.js"
+  },
+  {
+    "revision": "65a7704afd6c1827d693",
+    "url": "/react-udemy/static/js/main.a87d77eb.chunk.js"
+  },
+  {
+    "revision": "3dc75907c330b2f66f7f",
+    "url": "/react-udemy/static/js/runtime~main.993c86d4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nco71/react-udemy/precache-manifest.adefffc129d4b56dedb5c0f0614c2152.js"
+  "/react-udemy/precache-manifest.481f3ba548b047a70a39b75eb35c0bd9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nco71/react-udemy/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-udemy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
