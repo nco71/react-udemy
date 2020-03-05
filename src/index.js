5bfa43320a63acf492d42475e7dfe139c9607c4b
chaux cedric <chauxcedric@gmail.com>
2020-03-05 23:47:17 +0800

    user reducer and actions redux > header + app comp

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 
-import './index.css';
-import App from './App';
+import { Provider  } from 'react-redux'
+import store from './redux/store'
 
+import './index.css'
+import App from './App'
+
+const rootElement = document.getElementById('root')
 ReactDOM.render(
+ <Provider store={store}>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+  </BrowserRouter>
+  </Provider>,
+  rootElement
+)
