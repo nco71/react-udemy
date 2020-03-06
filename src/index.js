f7c84ad7ff63b8a34bbb2fe909c2becd9f1cc918
chaux cedric <chauxcedric@gmail.com>
2020-03-06 19:35:45 +0800

    selector for icon

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import { Provider  } from 'react-redux'
 import store from './redux/store'
 
 import './index.css'
+
 import App from './App'
+
 
 const rootElement = document.getElementById('root')
 ReactDOM.render(
