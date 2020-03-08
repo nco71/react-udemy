02596c493489af620be3081036858dd930898775
chaux cedric <chauxcedric@gmail.com>
2020-03-08 14:15:19 +0800

    added stripe

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
-
-//import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import  StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import {
   selectCartItems,
@@ -12,7 +11,7 @@ import {
 
 import './checkout.styles.scss';
 
-const CheckoutPage = ({ cartItems, total }) => (
+const CheckoutPage = ({ cartItems, total, nextMonth = new Date().getMonth() + 2 }) => (
   <div className='checkout-page'>
     <div className='checkout-header'>
       <div className='header-block'>
@@ -35,6 +34,12 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className='total'>TOTAL: ${total}</div>
+    <div className='test-warning'>
+    *Please use the following test credit card for payments*
+    <br />
+    4242 4242 4242 4242 - Exp: {nextMonth}/20 - CVV: 123
+  </div>
+  <StripeCheckoutButton price={total} />
   </div>
 );
 
