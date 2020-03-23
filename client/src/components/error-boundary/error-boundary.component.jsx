773130323f5434424163507c9dc98fabc5f68dae
chaux cedric <chauxcedric@gmail.com>
2020-03-23 12:19:05 +0800

    optimizatio with lazy and suspense

---
+++ client/src/components/error-boundary/error-boundary.component.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+    ErrorImageContainer,
+    ErrorImageOverlay,
+    ErrorImageText
+} from './error-boundary.styles';
+
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { hasError: false };
+    }
+  
+   static getDerivedStateFromError(error) { 
+        return { hasError: true }; 
+    }
+  componentDidCatch(error, errorInfo) {
+     console.log(error, errorInfo); 
+  }
+    render() {
+      if (this.state.hasError) {
+        return (
+             <ErrorImageOverlay>
+                <ErrorImageContainer imageUrl='https://i.imgur.com/yW2W9SC.png' />
+                 <ErrorImageText>Sorry, Something went wrong</ErrorImageText>
+             </ErrorImageOverlay>
+        )
+    }
+      return this.props.children; 
+    };
+  }
+
+  export default ErrorBoundary;
